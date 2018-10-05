9c15195b7f2b82ca2706fb85295282d0fc15a033
seung8 <lsy0914@naver.com>
2018-10-05 17:46:21 +0900

    기능 테스트 주석 추가

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,11 @@
 from selenium import webdriver
 
 browser = webdriver.Chrome()
+
+# To-Do 리스트 웹 페이지 열기
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# 웹 페이지 타이틀과 헤더에 'To-Do'를 명시하는지(해당 웹 페이지가 맞는지) 확인
+assert 'To-Do' in browser.title
+
+browser.quit()
