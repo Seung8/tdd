c8b563c38fd22ebce48f09fa9070c72628f47652
seung8 <lsy0914@naver.com>
2018-10-06 21:03:53 +0900

    To-Do 아이템 추가 기능 테스트

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,7 @@
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -18,9 +19,26 @@ class NewVisitorTest(unittest.TestCase):
 
     # 웹 페이지를 열고 해당 페이지가 맞는 지 테스트
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
-
+        self.browser.get('http://localhost:8001')
         self.assertIn('To-Do', self.browser.title)
+
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            '작업 아이템 입력'
+        )
+
+        inputbox.send_keys('공작깃털 사기')
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: 공작깃털 사기' for row in rows)
+        )
         self.fail('Finish the test!')
 
 
