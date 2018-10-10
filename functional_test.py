4a1d9d60474836003c1245a626a85ffd725266e1
seung8 <lsy0914@naver.com>
2018-10-10 18:44:53 +0900

    사용자 인풋 테스트 추가 - 템플릿 작업 목록 임의 넘버링으로 고의 에러 발생

--- functional_test.py
+++ functional_test.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import unittest
 
 from selenium import webdriver
@@ -36,10 +38,8 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: 공작깃털 사기' for row in rows),
-            "신규 작업이 테이블에 표시되지 않는다"
-        )
+        self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
+        self.assertIn('2: 공작깃털을 이용해서 그물 만들기', [row.text for row in rows])
         self.fail('Finish the test!')
 
 
