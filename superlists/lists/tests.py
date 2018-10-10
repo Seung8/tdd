a0150b261743af31cf88bb59e2026b94e991f7d0
seung8 <lsy0914@naver.com>
2018-10-11 05:56:42 +0900

    Django 2.0에 맞게 lists 단위 테스트 로직 수정(observed_html), 모델 생성 테스트 추가

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,8 +1,12 @@
+import re
+
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
 from django.urls import resolve
+
 from .views import home_page
+from .models import Item
 
 
 class HomePageTest(TestCase):
@@ -13,18 +17,40 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        observed_html = re.sub(csrf_regex, '', response.content.decode())
         expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(observed_html, expected_html)
 
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
         request.method = 'POST'
         request.POST['item_text'] = '신규 작업 아이템'
-
         response = home_page(request)
         self.assertIn('신규 작업 아이템', response.content.decode())
+
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        observed_html = re.sub(csrf_regex, '', response.content.decode())
         expected_html = render_to_string(
             'home.html',
-            {'new_item_text': '신규 작업 아이템'}
+            {'new_item_text': '신규 작업 아이템'},
         )
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(observed_html, expected_html)
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = '첫 번째 아이템'
+        first_item.save()
+
+        second_itme = Item()
+        second_itme.text = '두 번째 아이템'
+        second_itme.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, '첫 번째 아이템')
+        self.assertEqual(second_saved_item.text, '두 번째 아이템')
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 
 def home_page(request):
